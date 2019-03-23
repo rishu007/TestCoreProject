071c5c5a9e355ea4773a81e5fbbd4001ded7abc0
rishu007 <rishulakhotiya@gmail.com>
2019-03-23 22:00:02 +0530

    to do controller

---
+++ TestCore/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
